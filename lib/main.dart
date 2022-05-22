e1061c8262c52c89b604b8397878c52c7444f6d3
Avinashvns <83054042+Avinashvns@users.noreply.github.com>
2022-05-22 13:13:19 +0530

    Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,23 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("App Bar Widget"),
-        backgroundColor: Colors.purple.withOpacity(.50),
-      ),
-      body: Center(
-        child: Image(
-          image: AssetImage(
-              "asset/akshay.jpg"
-          ),
-        ),
-      ),
-    ),
-  ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(
+         title: Text("Stateless Widget"),
+         backgroundColor: Colors.purple.withOpacity(.50),
+       ),
+       body: Center(
+         child: Text("Welcome in my App"),
+       ),
+     ),
+   );
+  }
+
 }
